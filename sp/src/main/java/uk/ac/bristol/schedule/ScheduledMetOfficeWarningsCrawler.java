ed2f0f6a8877ea81c413fe26a7830d7cd4f1c783
soyo <KatieIove@outlook.com>
2025-07-02 18:00:22 +0100

    new mock data

--- sp/src/main/java/uk/ac/bristol/schedule/ScheduledMetOfficeWarningsCrawler.java
+++ sp/src/main/java/uk/ac/bristol/schedule/ScheduledMetOfficeWarningsCrawler.java
@@ -2,7 +2,6 @@ package uk.ac.bristol.schedule;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
