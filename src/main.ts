2b60afbc89ddc505341f557723ef788d166bbeba
zr <1628896010@qq.com>
2025-07-06 20:16:55 +0100

    fix message style

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import pinia from '@/stores'
 import '@/assets/main.scss'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 app.use(pinia)
