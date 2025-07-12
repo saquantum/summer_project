bc215569d2d02a4155c97556a52ad6c86432bee9
YDIAN <alchemistbackydian@gmail.com>
2025-07-12 23:26:39 +0100

    adjust

--- sp/src/main/java/uk/ac/bristol/service/impl/UserServiceImpl.java
+++ sp/src/main/java/uk/ac/bristol/service/impl/UserServiceImpl.java
@@ -489,7 +489,7 @@ public class UserServiceImpl implements UserService {
     private String hashPassword(String password) {
         Argon2Function argon2 = Argon2Function.getInstance(
                 19456,
-                2,
+                1,
                 1,
                 256,
                 Argon2.ID
@@ -505,7 +505,7 @@ public class UserServiceImpl implements UserService {
     private boolean verifyPassword(String plainPassword, String hashedPassword) {
         Argon2Function argon2 = Argon2Function.getInstance(
                 19456,
-                2,
+                1,
                 1,
                 256,
                 Argon2.ID
