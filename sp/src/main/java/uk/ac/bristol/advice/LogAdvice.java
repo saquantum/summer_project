c850ee12044bf154e5f6a1aaf9a189985d2141d2
soyo <KatieIove@outlook.com>
2025-07-28 16:49:23 +0100

    some mics optimisations

--- sp/src/main/java/uk/ac/bristol/advice/LogAdvice.java
+++ sp/src/main/java/uk/ac/bristol/advice/LogAdvice.java
@@ -15,29 +15,37 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 // TODO: replace creating io streams to thread pool
 
 @Component
 @Aspect
 public class LogAdvice {
+    private static final ExecutorService threadPool = Executors.newFixedThreadPool(4);
+
     @Pointcut("execution(* *..service..*Service.*(..))")
     public void service() {
     }
 
     @After("service()")
     public void serviceLogger(JoinPoint jp) throws IOException {
-        try (BufferedOutputStream bos = getLogFileStream()) {
-            StringBuilder sb = new StringBuilder()
-                    .append(TraceThreadLocalID.get()).append("\t")
-                    .append(LocalDateTime.now()).append("\t")
-                    .append(jp.getSignature().getDeclaringTypeName()).append(".")
-                    .append(jp.getSignature().getName())
-                    .append(Arrays.toString(jp.getArgs())).append("\t")
-                    .append(System.lineSeparator());
-            bos.write(sb.toString().getBytes());
-            bos.flush();
-        }
+        threadPool.submit(() -> {
+            try (BufferedOutputStream bos = getLogFileStream()) {
+                StringBuilder sb = new StringBuilder()
+                        .append(TraceThreadLocalID.get()).append("\t")
+                        .append(LocalDateTime.now()).append("\t")
+                        .append(jp.getSignature().getDeclaringTypeName()).append(".")
+                        .append(jp.getSignature().getName())
+                        .append(Arrays.toString(jp.getArgs())).append("\t")
+                        .append(System.lineSeparator());
+                bos.write(sb.toString().getBytes());
+                bos.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     @Pointcut("execution(* *..controller..*Controller.*(..))")
@@ -49,28 +57,27 @@ public class LogAdvice {
         try (BufferedOutputStream bos = getLogFileStream()) {
             String traceId = UUID.randomUUID().toString();
             TraceThreadLocalID.set(traceId);
-            StringBuilder before = new StringBuilder()
-                    .append(traceId).append("\t")
-                    .append(LocalDateTime.now()).append("\t")
-                    .append(jp.getSignature().getDeclaringTypeName()).append(".")
-                    .append(jp.getSignature().getName())
-                    .append(Arrays.toString(jp.getArgs())).append("\t")
-                    .append(System.lineSeparator());
-            bos.write(before.toString().getBytes());
-            bos.flush();
-
+            threadPool.submit(() -> logControllerMethod("before", jp, traceId));
             Object result = jp.proceed();
+            threadPool.submit(() -> logControllerMethod("after", jp, traceId));
+            return result;
+        }
+    }
 
-            StringBuilder after = new StringBuilder()
+    private void logControllerMethod(String stage, ProceedingJoinPoint jp, String traceId) {
+        try (BufferedOutputStream bos = getLogFileStream()) {
+            StringBuilder sb = new StringBuilder()
                     .append(traceId).append("\t")
                     .append(LocalDateTime.now()).append("\t")
+                    .append(stage).append("\t")
                     .append(jp.getSignature().getDeclaringTypeName()).append(".")
                     .append(jp.getSignature().getName())
                     .append(Arrays.toString(jp.getArgs())).append("\t")
                     .append(System.lineSeparator());
-            bos.write(after.toString().getBytes());
+            bos.write(sb.toString().getBytes());
             bos.flush();
-            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
